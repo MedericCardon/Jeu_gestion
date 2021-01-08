b33cbf24032a9b4105e233cd6ad6256fc0c9cd83
MedericCardon <74670080+MedericCardon@users.noreply.github.com>
2021-01-08 18:10:15 +0100

    Update Jeu_gestion.py

--- Jeu_gestion.py
+++ Jeu_gestion.py
@@ -14,7 +14,9 @@ class Parcelle_1 :
         self.__choix = 0
         self.__grille = [".",".",".",".",".",".",".",".","."]
     def grille_parcelle (self):
-        print(self.__type)
+        print("")
+        print("   ",self.__type)
+        print("")
         print("=================")
         print("||",self.__grille[0],"||",self.__grille[1],"||",self.__grille[2],"||")
         print("=================")
@@ -22,8 +24,18 @@ class Parcelle_1 :
         print("=================")
         print("||",self.__grille[6],"||",self.__grille[7],"||",self.__grille[8],"||")
         print("=================")
+        print("")
+        print("")
     def changement_etat (self):
-        self.__choix = int(input("1) Planter des graine"))
+        print("")
+        print("1) Planter des graines dans la parcelle ? ")
+        print("2) Arroser votre parcelle ? ")
+        print("3) Mettre de l'engrais ? ")
+        print("4) Récolter la parcelle ? ")
+        print("")
+        self.__choix = int(input("Que voulez vous faire ? "))
+        print("")
+        print("")
         if self.__choix == 1 :
             self.__etat = 1
     def etat_parcelle (self):
@@ -31,50 +43,6 @@ class Parcelle_1 :
             print("Vous avez plantez des graines dans votre parcelle")
             self.__grille = ["*","*","*","*","*","*","*","*","*"]
             
-    
-
-
-
-
-
-class Parcelle :
-    def __init__ (self,nom_parcelle):
-        self.__nom = nom_parcelle
-        self.__repos_parcelle = False
-        self.__etat = 0
-    def choix_joueur (self):
-        while self.__etat < 5 :
-            print("1) Planter des graines")
-            print("2) Arroser votre parcelle")
-            print("3) Récolter votre parcelle")
-            print("4) Aller dormir")
-            print("")
-            print("")
-            self.__choix = int(input("Que voulez vous faire ?"))
-            if self.__choix == 1 :
-                print("Vous venez de planter des graines dans votre parcelle")
-                print("")
-                self.__etat = 1
-
-            if self.__etat < 2 and self.__choix ==2:
-                print("Vous ne pouvez pas arroser votre parcelle")
-                print("")
-                if self.__choix == 2 and self.__etat > 2:
-                    print("Vous venez d'arroser votre parcelle")
-                    print("")
-                
-
-            if self.__choix == 3 :
-                print("Vous avez récolté votre parcelle")
-                print("")
-
-            if self.__choix == 4 :
-                print("Vous allez vous coucher")
-                print("")
-                self.__etat = 2 
-    
-
-
 
 
 
